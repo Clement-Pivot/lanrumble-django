e324f925ebb80112fd424720f6a2140a69e8a443
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-11-07 00:28:58 +0100

    test: create fixture 🚨

--- api/tests/test_videogame.py
+++ api/tests/test_videogame.py
@@ -3,20 +3,11 @@ import logging
 
 
 class TestViewLogged(TestSetupLoged):
-    @classmethod
-    def setUpTestData(cls):
-        create_game(
-            title="Helldivers 2",
-            coop=True,
-            pvp=False,
-            max_hot_seat_players=1,
-            max_online_players=4,
-            f2p=False,
-        )
+    fixtures = ["api/fixtures/dump.json"]
 
     def test_get_game_1(self):
         request = self.client.get(
-            "/api/videogame/1",
+            "/api/videogame/75",
             content_type="application/json",
             secure=True,
         )
